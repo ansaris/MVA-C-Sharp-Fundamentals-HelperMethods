26890fdbd2ca2f305343eaf787e3d62ef36d4688
ansaris <salman.ansari.usa@gmail.com>
2016-04-23 15:06:10 -0500

    Removed unnecessary stuff.

--- HelperMethods/Program.cs
+++ HelperMethods/Program.cs
@@ -16,71 +16,17 @@ namespace HelperMethods
             Console.Write("What's your first name? ");
             string firstName = Console.ReadLine();
 
-            //Console.WriteLine();
             Console.Write("What's your last name? ");
             string lastName = Console.ReadLine();
 
-            //Console.WriteLine();
             Console.Write("In what city were you born? ");
             string birthCity = Console.ReadLine();
-            /*
-            char[] firstNameArray = firstName.ToCharArray();
-            char[] lastNameArray = lastName.ToCharArray();
-            char[] birthCityArray = birthCity.ToCharArray();
 
-            Array.Reverse(firstNameArray);
-            Array.Reverse(lastNameArray);
-            Array.Reverse(birthCityArray);
-            */
-            /*
-            string firstNameReversed = new string(firstNameArray);
-            string lastNameReversed = new string(lastNameArray);
-            string birthCityReversed = new string(birthCityArray);
-
-            Console.WriteLine(firstNameArray);
-            Console.WriteLine("Results: {0} {1} {2}", firstNameReversed, lastNameReversed, birthCityReversed);
-            //Console.WriteLine(birthCityArray.Reverse() + " " 
-            //+ lastNameArray.Reverse() + " " + firstNameArray.Reverse());
-            */
-            /*
-            string result = String.Format("{0} {1} {2}", 
-                ReverseString(firstName), ReverseString(lastName), ReverseString(birthCity));
-                */
-            /*
-            string firstNameReversed = ReverseString(firstName);
-            string lastNameReversed = ReverseString(lastName);
-            string birthCityReversed = ReverseString(birthCity);
-            */
             DisplayResult(
                 ReverseString(firstName),
                 ReverseString(lastName),
                 ReverseString(birthCity)
                 );
-
-            DisplayResult(ReverseString(firstName) + " " 
-                + ReverseString(lastName) + " " 
-                + ReverseString(birthCity));
-            /*
-            foreach (char character in firstNameArray)
-            {
-                result += character;
-            }
-
-            result += " ";
-
-            foreach (char character in lastNameArray)
-            {
-                result += character;
-            }
-
-            result += " ";
-
-            foreach (char character in birthCityArray)
-            {
-                result += character;
-            }
-            */
-            //Console.WriteLine("Results: {0}", result);
 
             Console.ReadKey();
 
@@ -90,7 +36,6 @@ namespace HelperMethods
         {
             char[] originalArray = originalString.ToCharArray();
             Array.Reverse(originalArray);
-            // string reversedString = new string(originalArray);
             return String.Concat(originalArray);
         }
 
